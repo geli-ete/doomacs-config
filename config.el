6c195bc7eb2c381d68b2df4aae0200b2348da457
Geli Ete <g.geliete@gmail.com>
2023-12-31 22:42:00 +0530

    Org agenda and roam are set

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
- (setq user-full-name "Geli Ete"
-       user-mail-address "emacsgeli@gmail.com")
+(setq user-full-name "Geli Ete"
+      user-mail-address "emacsgeli@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -43,7 +43,201 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/org-files/")
+(setq org-agenda-files '("~/Dropbox/org-files/agenda/school.org"
+                         "~/Dropbox/org-files/agenda/personal.org"
+                         "~/Dropbox/org-files/agenda/projects.org"
+                         "~/Dropbox/org-files/agenda/journal.org"
+                         "~/Dropbox/org-files/agenda/tasks.org"))
+
+
+;; Set `org-agenda-files'
+;; `org-agenda' Setup After Package Load
+(after! org
+  (setq org-agenda-span 'week)
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-reschedule 'time)
+  (setq org-log-into-drawer t)
+
+  ;; Org Todo Keywords
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+          (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+  ;; Org Agenda Custom Commands
+  (setq org-agenda-custom-commands
+        '(("d" "Dashboard"
+           ((agenda "" ((org-deadline-warning-days 7)))
+            (todo "NEXT" ((org-agenda-overriding-header "Next Tasks")))
+            (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+          ("n" "Next Tasks" ((todo "NEXT" ((org-agenda-overriding-header "Next Tasks")))))
+          ("W" "Work Tasks" tags-todo "+work-email")
+          ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+           ((org-agenda-overriding-header "Low Effort Tasks") (org-agenda-max-todos 20)))
+          ("w" "Workflow Status"
+           ((todo "WAIT" ((org-agenda-overriding-header "Waiting on External")))
+            (todo "REVIEW" ((org-agenda-overriding-header "In Review")))
+            (todo "PLAN" ((org-agenda-overriding-header "In Planning") (org-agenda-todo-list-sublevels nil)))
+            (todo "BACKLOG" ((org-agenda-overriding-header "Project Backlog") (org-agenda-todo-list-sublevels nil)))
+            (todo "READY" ((org-agenda-overriding-header "Ready for Work")))
+            (todo "ACTIVE" ((org-agenda-overriding-header "Active Projects")))
+            (todo "COMPLETED" ((org-agenda-overriding-header "Completed Projects")))
+            (todo "CANC" ((org-agenda-overriding-header "Cancelled Projects")))))))
+
+  (after! org
+    ;; Here we are setting `org-agenda-capture' as per our needs.
+    ;; `%?' places the cursor after the template is filled.
+    ;; `%U' inserts a timestamp.
+    ;; `%a' includes a link to the current location (if capturing from within Emacs).
+    ;; `%i' pulls in any selected text.
+    ;; `:prepend' t adds new entries at the beginning of the file.
+    (setq org-capture-templates
+          '(
+            ;; Add your templates here
+            ("t" "Any Task" entry (file+olp+datetree "~/Dropbox/org-files/agenda/tasks.org")
+             "* TODO %?\n  Created On: %U\n  Link: %a\n  Notes: %i" :prepend t)
+
+            ("s" "School Task" entry (file+olp+datetree "~/Dropbox/org-files/agenda/school.org")
+             "* TODO %?\n  Created On: %U\n  Link: %a\n  Notes: %i" :prepend t)
+
+            ("p" "Personal Task" entry (file+olp+datetree "~/Dropbox/org-files/agenda/personal.org")
+             "* TODO %?\n  Created On: %U\n  Link: %a\n  Notes: %i" :prepend t)
+
+            ("r" "Project Task" entry (file+olp+datetree "~/Dropbox/org-files/agenda/projects.org")
+             "* TODO %?\n  DEADLINE: %^t\n  Created On: %U\n  Link: %a\n  Notes: %i" :prepend t)
+
+            ("j" "Journal Entry" entry (file+olp+datetree "~/Dropbox/org-files/agenda/journal.org")
+             "* %?\n  Entered on %U\n Notes: %i" :prepend t :clock-in :clock-resume
+             :empty-lines 1)
+
+            ("b" "Birthday" entry (file "~/Dropbox/org-files/agenda/birthdays.org")
+             "* %^t %?\n  Captured on: %U" :prepend t)
+
+            ("h" "Habit" entry (file+olp+datetree "~/Dropbox/org-files/agenda/habits.org")
+             "* TODO %?\n  SCHEDULED: <%<%Y-%m-%d %a .+1d>>\n  :PROPERTIES:\n  :STYLE: habit\n  :END:\n  Created On: %U Notes: %i" :prepend t)
+
+            ))))
+
+;; Setting `org-roam' directory using `org-roam-directory'.
+(setq org-roam-directory (file-truename "~/Dropbox/org-files/roam"))
+;; Org-Roam Setup After Package Load
+(after! org-roam
+  ;; Define Org-Roam Capture Templates
+  (setq org-roam-capture-templates
+        '(("d" "default" plain "%?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
+           :unnarrowed t)
+          ("l" "programming language" plain
+           "* Characteristics\n\n - Family: %?\n- Inspired by: \n\n* References:\n\n"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("b" "book notes" plain
+           (file "~/Dropbox/RoamNotes/Templates/BookNoteTemplate.org")
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+           :unnarrowed t)
+          ("p" "project" plain
+           "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
+           :unnarrowed t)          ;; Add more templates as needed
+          ))
+  ;; Setting Up Daily Notes
+  (setq org-roam-dailies-directory "daily/")
+  (setq org-roam-dailies-capture-templates
+        '(("d" "default" entry "* %<%I:%M %p>: %?"
+           :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  :config
+  (require 'org-roam-dailies)
+  (org-roam-db-autosync-mode)
+  )
+
+;; Custom functions for Org-roam
+(require 'cl-lib)
+
+(defun org-roam-node-insert-immediate (arg &rest args)
+  "Insert an Org-roam node immediately with given ARG and ARGS."
+  (interactive "P")
+  (let ((org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert arg args)))
+
+(defun my/org-roam-refresh-agenda-list ()
+  "Update `org-agenda-files` to include Org-roam files tagged with 'Project'."
+  (interactive)
+  (let ((project-files (my/org-roam-list-notes-by-tag "Project")))
+    (when project-files
+      (setq org-agenda-files (append org-agenda-files project-files)))))
+
+(defun my/org-roam-daily-refresh-agenda-list ()
+  "Refresh agenda list once daily."
+  (run-at-time "00:01" 86400 'my/org-roam-refresh-agenda-list))
+
+;; Call the function to set up the daily refresh.
+(my/org-roam-daily-refresh-agenda-list)
+
+(defun my/org-roam-filter-by-tag (tag-name)
+  "Return a function to filter Org-roam nodes by a given TAG-NAME."
+  (lambda (node)
+    (member tag-name (org-roam-node-tags node))))
+
+(defun my/org-roam-list-notes-by-tag (tag-name)
+  "Return a list of Org-roam file paths that contain the given TAG-NAME."
+  (when (fboundp 'org-roam-node-list)
+    (mapcar #'org-roam-node-file
+            (seq-filter (lambda (node) (member tag-name (org-roam-node-tags node)))
+                        (org-roam-node-list)))))
+(defun my/org-roam-project-finalize-hook ()
+  "Adds the captured project file to `org-agenda-files' if the capture was not aborted."
+  (remove-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+  (unless org-note-abort
+    (with-current-buffer (org-capture-get :buffer)
+      (add-to-list 'org-agenda-files (buffer-file-name)))))
+
+(defun my/org-roam-find-project ()
+  (interactive)
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+  (let ((org-roam-capture-templates
+         '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+            :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
+            :unnarrowed t))))
+    (org-roam-node-find nil nil (my/org-roam-filter-by-tag "Project"))))
+
+(defun my/org-roam-capture-inbox ()
+  (interactive)
+  (org-roam-capture- :node (org-roam-node-create)
+                     :templates '(("i" "inbox" plain "* %?"
+                                   :if-new (file+head "Inbox.org" "#+title: Inbox\n")))))
+
+(defun my/org-roam-capture-task ()
+  (interactive)
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+  (org-roam-capture- :node (org-roam-node-read nil (my/org-roam-filter-by-tag "Project"))
+                     :templates '(("p" "project" plain "** TODO %?"
+                                   :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
+                                                          "#+title: ${title}\n#+category: ${title}\n#+filetags: Project"
+                                                          ("Tasks"))))))
+
+(defun my/org-roam-copy-todo-to-today ()
+  (interactive)
+  (let ((org-refile-keep t)
+        (org-roam-dailies-capture-templates
+         '(("t" "tasks" entry "%?"
+            :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
+        today-file pos)
+    (save-window-excursion
+      (org-roam-dailies--capture (current-time) t)
+      (setq today-file (buffer-file-name))
+      (setq pos (point))
+      (unless (equal (file-truename today-file) (file-truename (buffer-file-name)))
+        (org-refile nil nil (list "Tasks" today-file nil pos)))))
+
+  (add-to-list 'org-after-todo-state-change-hook
+               (lambda ()
+                 (when (equal org-state "DONE")
+                   (my/org-roam-copy-todo-to-today)))))
+
+
+
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
--- init.el
+++ init.el
@@ -39,18 +39,18 @@
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash         ; blink cursor line after big motions
-       ;;neotree           ; a project drawer, like NERDTree for vim
+       nav-flash         ; blink cursor line after big motions
+       neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       tabs              ; a tab bar for Emacs
+       ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       ;;workspaces        ; tab emulation, persistence & separate workspaces
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -69,7 +69,7 @@
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -77,12 +77,12 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;(spell +flyspell) ; tasing you for misspelling mispelling
-       ;;grammar           ; tasing grammar mistake every you make
+       (spell +flyspell) ; tasing you for misspelling mispelling
+       grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
@@ -90,9 +90,9 @@
        ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
@@ -100,7 +100,7 @@
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       ;;pdf               ; pdf enhancements
+       pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
@@ -155,6 +155,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        org               ; organize your plain life in plain text
+       (org +roam2)        ; Level up your your organization with powerful org roam 2
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
